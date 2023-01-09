c669ce8417cde5319c19562ebec797a3a972242b
Anton Spiridonov <kerni2@ya.ru>
2023-01-09 13:57:14 +0300

    edit link model

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe Link, type: :model do
 
   it { should validate_presence_of :name }
   it { should validate_presence_of :url }
+
+  it "gist? should be true" do
+    @link = Link.new()
+    @link.name = "MyLink"
+    @link.url = "https://gist.github.com/kerni2/36e1d17e7764bb003727496feb417c1d"
+    expect(@link.gist?).to be true
+  end
+
 end
